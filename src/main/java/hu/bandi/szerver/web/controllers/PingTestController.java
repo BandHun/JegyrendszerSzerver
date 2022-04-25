9403ed214e6b65c94135b5a5523a56f0249f58bc
bandi <totthbandus@gmail.com>
2022-04-25 15:47:51 +0200

    Ping test controller

---
+++ src/main/java/hu/bandi/szerver/web/controllers/PingTestController.java
@@ -0,0 +1,17 @@
+package hu.bandi.szerver.web.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class PingTestController {
+    @GetMapping("/ping")
+    public ResponseEntity<String> getCommentById(@PathVariable("id") final Long id) {
+        return new ResponseEntity<>("Pinged: " + id, HttpStatus.OK);
+    }
+}
