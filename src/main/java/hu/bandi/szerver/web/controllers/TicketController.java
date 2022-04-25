1c7751f12b3250221bef790c717a6960983096fa
bandi <totthbandus@gmail.com>
2022-04-25 11:46:43 +0200

    First controller to test

---
+++ src/main/java/hu/bandi/szerver/web/controllers/TicketController.java
@@ -0,0 +1,48 @@
+package hu.bandi.szerver.web.controllers;
+
+import hu.bandi.szerver.models.Ticket;
+import hu.bandi.szerver.services.interfaces.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ticket")
+public class TicketController {
+
+    @Autowired
+    private final TicketService ticketService;
+
+    public TicketController(final TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Ticket>> getAllTicket() {
+        return new ResponseEntity<>(ticketService.findAllTickets(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Ticket> getTicketById(@PathVariable("id") final Long id) {
+        return new ResponseEntity<>(ticketService.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Ticket> addTicket(@RequestBody final Ticket ticket) {
+        return new ResponseEntity<>(ticketService.addTicket(ticket), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Ticket> updateTicket(@RequestBody final Ticket ticket) {
+        return new ResponseEntity<>(ticketService.updateTicket(ticket), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteTicket(@PathVariable("id") final Long id) {
+        ticketService.deleteTicket(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
