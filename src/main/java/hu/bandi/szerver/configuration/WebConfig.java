876291fef7877bc38990e370d4f49f293754f56e
bandi <totthbandus@gmail.com>
2022-04-25 22:54:18 +0200

    Config fix

--- src/main/java/hu/bandi/szerver/configuration/WebConfig.java
+++ src/main/java/hu/bandi/szerver/configuration/WebConfig.java
@@ -1,39 +1,32 @@
 package hu.bandi.szerver.configuration;
 
 import hu.bandi.szerver.services.interfaces.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-@EnableWebMvc
-public class WebConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+@EnableWebSecurity
+public class WebConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    public void addCorsMappings(final CorsRegistry registry) {
-        registry.addMapping("/**");
+
+    private final UserService userService;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public WebConfig(final UserService userService) {
+        this.userService = userService;
+        passwordEncoder = new BCryptPasswordEncoder();
     }
-
-
-    @Autowired
-    private UserService userService;
 
     @Bean
     public static BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Autowired
-    private BCryptPasswordEncoder passwordEncoder;
 
     @Override
     protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
